43d1238427bea10558c092a85f2bf53261938b09
Duckiee <duc.nc@atomsolution.com.vn>
2025-12-05 15:57:20 +0700

    feat: update git hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute('/')({
 })
 
 function RouteComponent() {
-  return <div className='text-xl'>Hello "/"!</div>
+  return <div className='text-2xl'>Hello "/"!</div>
 }
